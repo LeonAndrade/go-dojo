0192f4623e841de677805b793fb0497249bedf3b
LeonAndrade <35314330+LeonAndrade@users.noreply.github.com>
2023-12-31 15:13:20 -0300

    Adding path validation to handlers
    Protecting simple vulnerabilities

--- wiki/wiki.go
+++ wiki/wiki.go
@@ -1,57 +1,100 @@
-package main 
+package main
 
 import (
-  "fmt"
-  "os"
-  "log"
-  "net/http"
-  "html/template"
+	"fmt"
+  "errors"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+  "regexp"
 )
 
 type Page struct {
-  Title string
-  Body []byte
+	Title string
+	Body  []byte
 }
 
 func (p *Page) save() error {
-  filename := p.Title + ".txt"
-  return os.WriteFile(filename, p.Body, 0600)
+	filename := p.Title + ".txt"
+	return os.WriteFile(filename, p.Body, 0600)
+}
+
+var templates = template.Must(template.ParseFiles(
+	"pages/edit.html",
+	"pages/view.html",
+))
+
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+    m := validPath.FindStringSubmatch(r.URL.Path)
+    if m == nil {
+      http.NotFound(w, r)
+      return "", errors.New("Invalid Page Title")
+  }
+  return m[2], nil
 }
 
 func loadPage(title string) (*Page, error) {
-  filename := title + ".txt"
-  body, err := os.ReadFile(filename)
-  if err != nil {
-    return nil, err
-  }
-  return &Page{Title: title, Body: body}, nil
+	filename := title + ".txt"
+	body, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	err := templates.ExecuteTemplate(w, "pages/"+tmpl+".html", p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-  title := r.URL.Path[len("/view/"):]
-  p, _ := loadPage(title)
-  t, _ := template.ParseFiles("pages/view.html")
-  t.Execute(w, p)
+  title, err := getTitle(w, r)
+  if err != nil {
+    return
+  }
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+	renderTemplate(w, "view", p)
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
-  title := r.URL.Path[len("/edit/"):]
-  p, err := loadPage(title)
+  title, err := getTitle(w, r)
   if err != nil {
-    p = &Page{Title: title}
+    return
   }
-  t, _ := template.ParseFiles("pages/edit.html")
-  t.Execute(w, p)
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+  title, err := getTitle(w, r)
+  if err != nil {
+    return
+  }
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
 func main() {
-  http.HandleFunc("/", handler)
-  http.HandleFunc("/view/", viewHandler)
-  http.HandleFunc("/edit/", editHandler)
-  // http.HandleFunc("/save/", saveHandler)
-  log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	// http.HandleFunc("/save/", saveHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
